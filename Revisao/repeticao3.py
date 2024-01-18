5bc3d9f42711c2ef92fb55950ea17e3a4c752f99
Senac Tatuape <senac@localhost.localdomain>
2024-01-18 11:21:02 -0300

    Estrutura de repetição

---
+++ Revisao/repeticao3.py
@@ -0,0 +1,11 @@
+list1 = [10,6,38,2,5]
+list2 = [44,0,8,2,6,80]
+list3 = []
+
+
+
+for i in list1:
+    for j in list2:
+        if i == j:
+            list3.append(i)
+print("Os números iguais são "+str(list3))            
